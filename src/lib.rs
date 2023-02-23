325e77b94c799c9cfec04a7c6127b2558681a2fa
vier <kdbvier@gmail.com>
2023-02-23 03:00:12 -0800

    fix: max_supply added

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,7 @@ use near_sdk::{
     assert_one_yocto, env, log, near_bindgen, AccountId, Balance, PanicOnDefault, PromiseOrValue,
 };
 use std::convert::TryInto;
+use std::u128;
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
@@ -33,6 +34,7 @@ pub struct Contract {
     token: FungibleToken,
     owner_id: AccountId,
     metadata: LazyOption<FungibleTokenMetadata>,
+    max_supply: Balance
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
@@ -42,7 +44,7 @@ impl Contract {
     /// Initializes the contract with the given total supply owned by the given `owner_id` with
     /// default metadata (for example purposes only).
     #[init]
-    pub fn new_default_meta(owner_id: AccountId) -> Self {
+    pub fn new_default_meta(owner_id: AccountId, max_supply: Balance) -> Self {
         Self::new(
             owner_id,
             FungibleTokenMetadata {
@@ -54,6 +56,7 @@ impl Contract {
                 reference_hash: None,
                 decimals: 24,
             },
+            max_supply
         )
     }
 
@@ -75,25 +78,28 @@ impl Contract {
     /// Initializes the contract with the given total supply owned by the given `owner_id` with
     /// the given fungible token metadata.
     #[init]
-    pub fn new(owner_id: AccountId, metadata: FungibleTokenMetadata) -> Self {
+    pub fn new(owner_id: AccountId, metadata: FungibleTokenMetadata, max_supply: Balance) -> Self {
         assert!(!env::state_exists(), "Already initialized");
         metadata.assert_valid();
         let mut this = Self {
             token: FungibleToken::new(b"a".to_vec()),
             metadata: LazyOption::new(b"m".to_vec(), Some(&metadata)),
             owner_id: owner_id,
+            max_supply: max_supply
         };
         this
     }
 
     pub fn mint(&mut self, account_id: ValidAccountId, amount: U128) -> U128 {
-        assert_one_yocto();
+        // assert_one_yocto();
         // assert_eq!(false, true, "Revert");
         assert_eq!(
             env::predecessor_account_id(),
             self.owner_id,
             "ERR_NOT_ALLOWED"
         );
+        let next_total_supply:Balance = self.token.total_supply.checked_add(amount.into()).unwrap();
+        assert!(next_total_supply<=self.max_supply, "Overflow");
         let account = self.token.accounts.get(account_id.as_ref());
         if account == None {
             self.token.internal_register_account(account_id.as_ref());
@@ -114,6 +120,16 @@ impl Contract {
             .internal_withdraw(account_id.as_ref(), amount.into());
     }
 
+    pub fn change_max_supply(&mut self, max_supply: Balance) {
+        assert_one_yocto();
+        assert_eq!(
+            env::predecessor_account_id(),
+            self.owner_id,
+            "ERR_NOT_ALLOWED"
+        );
+        self.max_supply = max_supply;
+    }
+
     fn on_account_closed(&mut self, account_id: AccountId, balance: Balance) {
         log!("Closed @{} with {}", account_id, balance);
     }
@@ -129,7 +145,8 @@ near_contract_standards::impl_fungible_token_storage!(Contract, token);
 #[near_bindgen]
 impl FungibleTokenMetadataProvider for Contract {
     fn ft_metadata(&self) -> FungibleTokenMetadata {
-        unimplemented!()
+        let metadata = self.metadata.get().unwrap();
+        metadata
     }
 }
 
@@ -144,6 +161,7 @@ mod tests {
     fn test_basics() {
         let mut context = VMContextBuilder::new();
         testing_env!(context.build());
+        let max_supply:Balance = 210000;
         let mut contract = Contract::new(accounts(0).to_string(), {
             FungibleTokenMetadata {
                 spec: "ft-1.0.0".to_string(),
@@ -154,7 +172,7 @@ mod tests {
                 reference: None,
                 reference_hash: None,
             }
-        });
+        },max_supply);
         // testing_env!(context
         //     .predecessor_account_id(farmer)
         //     .is_view(false)
@@ -164,15 +182,15 @@ mod tests {
 
         testing_env!(context
             .attached_deposit(1)
-            .predecessor_account_id(accounts(1))
+            .predecessor_account_id(accounts(0))
             .build());
         // contract.mint(accounts(0), 1_000_000.into());
         // assert_eq!(contract.ft_balance_of(accounts(0)), 1_000_000.into());
-        contract.mint(accounts(1), 1_000_000.into());
-        println!("MintedValue: {:?}", contract.ft_balance_of(accounts(1)));
+        contract.change_max_supply(1_000_000);
+        contract.mint(accounts(0), 1_000_000.into());
+        println!("MintedValue: {:?}", contract.ft_balance_of(accounts(0)));
         // assert_eq!(contract.ft_balance_of(accounts(0)), 2_000_000.into());
         // contract.burn(accounts(0), 1_000_000.into());
-        // assert_eq!(contract.ft_balance_of(accounts(0)), 1_000_000.into());
 
         // testing_env!(context
         //     .attached_deposit(125 * env::storage_byte_cost())
